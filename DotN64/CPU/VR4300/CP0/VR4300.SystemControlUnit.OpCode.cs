8e076ff0aeb391abb585b4f3a850f8e04273ec51
Nabile Rahmani <nabile13.pc@gmail.com>
2018-01-17 17:30:01 +0100

    Moved CP0.OpCode enum.

---
+++ DotN64/CPU/VR4300/CP0/VR4300.SystemControlUnit.OpCode.cs
@@ -0,0 +1,16 @@
+﻿namespace DotN64.CPU
+{
+    public partial class VR4300
+    {
+        public partial class SystemControlUnit
+        {
+            public enum OpCode : byte
+            {
+                /// <summary>Move To System Control Coprocessor.</summary>
+                MT = 0b00100,
+                /// <summary>Move From System Control Coprocessor.</summary>
+                MF = 0b00000
+            }
+        }
+    }
+}
--- DotN64/CPU/VR4300/CP0/VR4300.SystemControlUnit.cs
+++ DotN64/CPU/VR4300/CP0/VR4300.SystemControlUnit.cs
@@ -63,14 +63,6 @@ namespace DotN64.CPU
                     ExceptionProcessing.ReservedInstruction(cpu, instruction);
             }
             #endregion
-
-            public enum OpCode : byte
-            {
-                /// <summary>Move To System Control Coprocessor.</summary>
-                MT = 0b00100,
-                /// <summary>Move From System Control Coprocessor.</summary>
-                MF = 0b00000
-            }
         }
     }
 }
